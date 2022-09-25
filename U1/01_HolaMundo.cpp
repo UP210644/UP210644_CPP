df5432a806f624e551916cf496c3cde9ebbb33b0
UP210644 <up210644@alumnos.upa.edu.mx>
2022-09-24 23:36:48 -0500

    Update 01_HolaMundo.cpp

---
+++ U1/01_HolaMundo.cpp
@@ -0,0 +1,15 @@
+/* Unidad 1. Hola mundo
+   Autor: Isaias Moreno Luna
+   Fecha: 22/09/2022
+   Descripción: Imprime en pantalla un texto 
+*/
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hola mundo\n";
+
+    return 0;
+}
