96bcb39fd6b23d3fd6652340ca127dc05413f410
UP210644 <up210644@alumnos.upa.edu.mx>
2022-09-28 10:02:19 -0500

    Update 01_IfSimple.cpp

--- U2/01_IfSimple.cpp
+++ U2/01_IfSimple.cpp
@@ -1,22 +1,34 @@
-/* Unidad 2. Uso del if 
+/* Unidad 2. Ejercicio sobre el uso del if 
    Autor: Isaias Moreno Luna
-   Fecha: 22/09/2022
-   Descripción: Muestra el uso del if simple
+   Fecha: 24/09/2022
+   Descripción: Programa con el uso del if
 */
 
 #include <iostream>
-
 using namespace std;
+
 int main()
 {
-   int numero;
-   int dato = 1;
+    int numero;
+    int dato = 1;
 
-   cout << "Ingresa un numero entero: ";
-   cin >> numero;
-   if (numero == dato){
-    cout << "El número es igual a dato" << endl;
-   }   
-   
+    cout<<"Ingresa un número entero:\n";
+    cin>>numero;
+    if (numero == dato){
+        cout<<"El número es igual a dato\n";
+    }
+
+    if (numero != dato){
+        cout<<"El número es diferente a dato\n";
+    }
+
+    if (numero < dato){
+        cout<<"El número es menor a dato\n";
+    }
+
+    if (numero > dato){
+        cout<<"El número es mayor a dato\n";
+    }
+
     return 0;
 }
